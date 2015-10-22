5243c3f0e3bc27a392eef32c7ed7237e099f2e03
Anthony Gonzalez <darkdevilishag@gmail.com>
2015-10-21 22:02:41 -0400

    Adds faker seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+99.times do |n|
+  full_name = Faker::Name.name
+  email_address = "example-#{n+1}@example.com"
+  phone_number = Faker::PhoneNumber
+  notes = Faker::Lorem.paragraph
+  Customer.create(full_name: full_name,
+                  email_address: email_address,
+                  phone_number: phone_number,
+                  notes: notes)
+end
