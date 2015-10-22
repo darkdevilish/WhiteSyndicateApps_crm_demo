86f53e8bfc27a8fc6b4e2f28bd9979441a280a44
Anthony Gonzalez <darkdevilishag@gmail.com>
2015-10-21 20:24:10 -0400

    Adds rails_admin with customer model

---
+++ app/models/customer.rb
@@ -0,0 +1,5 @@
+class Customer < ActiveRecord::Base
+  validates :full_name, presence: true
+  validates :phone_number, presence: true
+  validates :email_address, presence: true, uniqueness: true
+end
