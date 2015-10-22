d48001dc508cbb768b62c24a48dc8849d5fa2646
Anthony Gonzalez <darkdevilishag@gmail.com>
2015-10-21 20:31:56 -0400

    Adds route to index controller to redirect to admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def index
+    redirect_to rails_admin_path
+  end
 end
